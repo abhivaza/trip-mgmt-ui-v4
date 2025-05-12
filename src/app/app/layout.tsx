891e32e20b737b11d4489ef51675defe793eeb15
Abhishek Vaza <abhishekvaza@gmail.com>
2025-05-12 11:14:54 -0400

    Update layout.tsx

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -3,7 +3,9 @@ import { useRouter } from "next/navigation";
 import type React from "react";
 
 import { useAuth } from "@/providers/auth-provider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import LoadingSpinner from "@/components/loading-spinner";
 
 export default function ProtectedLayout({
   children,
@@ -12,15 +14,41 @@ export default function ProtectedLayout({
 }>) {
   const { user } = useAuth();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (!user) {
-      router.push("/");
-    }
-  }, [user, router]);
+    // wait for 2 seconds before redirecting
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }, [user]);
 
-  if (!user) {
-    return null;
+  if (isLoading && !user) {
+    return (
+      <div className="text-center mt-8">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (!isLoading && !user) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center  bg-gradient-to-br from-[#ffe4e6] to-[#dbeafe]">
+        <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold text-gray-900">Access Denied</h1>
+            <p className="mt-2 text-gray-600">
+              You need to be logged in to access this page.
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <Button onClick={() => router.push("/")} className="px-6 py-2">
+              Go to Home Page
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return <>{children}</>;
